ec2c17e6aecf7d1d7b7b3ff7343a2ec6d0a616f1
Athos Freitas <93884747+athoscf@users.noreply.github.com>
2022-12-19 15:51:31 +0000

    Update gatilho1_adiciona.sql

--- gatilho1_adiciona.sql
+++ gatilho1_adiciona.sql
@@ -4,5 +4,5 @@ for each row
 begin
     update Jogador set contagem_pessoal = contagem_pessoal + 1
     where Jogador.numero_jogador = new.numero_jogador and Jogador.nome_selecao = new.nome_selecao
-        and new.tipo <> "autogolo";
+        and new.tipo = "default";
 end;
