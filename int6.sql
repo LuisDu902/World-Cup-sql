891681ac6d5e357e8d1d37fd38c6ffdfb66a46fa
Sofia ViP <92641892+SofiaViP@users.noreply.github.com>
2022-12-19 15:21:04 +0000

    Update int6.sql

--- int6.sql
+++ int6.sql
@@ -1,7 +1,3 @@
-.mode columns
-.headers on
-.nullvalue NULL
-
 /*
 Interrogação 6
 --------------
@@ -9,10 +5,14 @@ Mostre a percentagem de partidas dos playoffs que foram a desempate.
 Evidencie a quantidade de partidas dos playoffs e a quantidade de partidas que foram a desempate.
 */
 
-select a.partidas_playoffs, b.partidas_desempate,
- b.partidas_desempate*1.0/a.partidas_playoffs * 100 as PERCENTAGEM from 
+.mode columns
+.headers on
+.nullvalue NULL
+
+select A.partidas_playoffs, B.partidas_desempate,
+ B.partidas_desempate*1.0/A.partidas_playoffs * 100 as PERCENTAGEM from 
 
 (select count(*) as PARTIDAS_PLAYOFFS from partida
-where numero_jornada > 3) as a,
+where numero_jornada > 3) as A,
 
-(select count(*) as PARTIDAS_DESEMPATE from Desempate) as b
+(select count(*) as PARTIDAS_DESEMPATE from Desempate) as B
