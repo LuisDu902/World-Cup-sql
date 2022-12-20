d183ba1c9bd2ad8b1fc135c54d815ddce5bb7b8b
LuisDu902 <93838954+LuisDu902@users.noreply.github.com>
2022-12-20 22:47:03 +0000

    Update gatilho2_adiciona.sql

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -2,7 +2,7 @@ CREATE TRIGGER gatilho2
 BEFORE INSERT ON Partida
 FOR EACH ROW
 WHEN new.nome_selecao_1 IN (SELECT nome_selecao FROM Selecao WHERE posicao_grupo > 2)
-OR new.nome_selecao_2 in (SELECT nome_selecao FROM Selecao WHERE posicao_grupo > 2)
+OR new.nome_selecao_2 IN (SELECT nome_selecao FROM Selecao WHERE posicao_grupo > 2)
 BEGIN
     SELECT RAISE(ABORT,"Uma seleção eliminada na fase de grupos não pode participar de uma partida da fase eliminatoria");
 END;
