8268f747bc1a310acdaa0b5bfb43649c07d5736f
Sofia ViP <92641892+SofiaViP@users.noreply.github.com>
2022-12-20 15:37:46 +0000

    Update int1.sql

--- int1.sql
+++ int1.sql
@@ -10,15 +10,10 @@ Mostre a média de golos de todas as seleções que não se apuraram para a fase
 
 SELECT AVG(soma_golos) AS MEDIA_GOLOS_SELECOES_ELIMINADAS 
 FROM (SELECT SUM(contagem_pessoal) AS soma_golos 
-    FROM (SELECT * 
-        FROM Selecao 
-        WHERE nome_selecao NOT IN (SELECT nome_selecao_1 FROM Partida WHERE numero_jornada = 4) 
-        AND 
-        nome_selecao NOT IN (SELECT nome_selecao_2 FROM Partida WHERE numero_jornada = 4)) AS Selecoes_elim
-    JOIN Jogador ON Selecoes_elim.nome_selecao = Jogador.nome_selecao
-    GROUP BY Jogador.nome_selecao)
-
-
-
-
- 
+      FROM (SELECT * 
+            FROM Selecao 
+            WHERE nome_selecao NOT IN (SELECT nome_selecao_1 FROM Partida WHERE numero_jornada = 4) 
+                  AND 
+            nome_selecao NOT IN (SELECT nome_selecao_2 FROM Partida WHERE numero_jornada = 4)) AS Selecoes_elim
+            JOIN Jogador ON Selecoes_elim.nome_selecao = Jogador.nome_selecao
+      GROUP BY Jogador.nome_selecao);
