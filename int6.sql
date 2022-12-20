c96f69eafe6f93fa34546c71e26f3bbac9dc8042
Sofia ViP <92641892+SofiaViP@users.noreply.github.com>
2022-12-20 15:33:53 +0000

    Update int6.sql

--- int6.sql
+++ int6.sql
@@ -9,10 +9,10 @@ Evidencie a quantidade de partidas dos playoffs e a quantidade de partidas que f
 .headers ON
 .nullvalue NULL
 
-SELECT A.partidas_playoffs, B.partidas_desempate,
- B.partidas_desempate*1.0/A.partidas_playoffs * 100 AS PERCENTAGEM FROM 
+SELECT A.partidas_playoffs, B.partidas_desempate, B.partidas_desempate*1.0/A.partidas_playoffs * 100 AS PERCENTAGEM
+FROM 
 
 (SELECT COUNT(*) AS PARTIDAS_PLAYOFFS FROM partida
 WHERE numero_jornada > 3) AS A,
 
-(SELECT COUNT(*) AS PARTIDAS_DESEMPATE FROM Desempate) AS B
+(SELECT COUNT(*) AS PARTIDAS_DESEMPATE FROM Desempate) AS B;
