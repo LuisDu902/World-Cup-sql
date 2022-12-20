b6ca26b766a4322dd99865d373c2f6a0951d0027
Sofia ViP <92641892+SofiaViP@users.noreply.github.com>
2022-12-20 16:24:37 +0000

    Add files via upload

--- int10.sql
+++ int10.sql
@@ -5,59 +5,21 @@ Mostre o nº de jogos ganho por cada seleção, ordenando pelo nome da seleção
 */
 
 .mode columns
-.headers ON
+.headers on
 .nullvalue NULL
-DROP VIEW IF EXISTS Empates;
-DROP VIEW IF EXISTS GolosDefault;
-DROP VIEW IF EXISTS Autogolos;
 
----------------------------------------------------------
-CREATE VIEW Empates As
-
-SELECT id_partida AS PARTIDA
+SELECT SELECAO, count(*) AS VITÓRIAS
+FROM
+(SELECT PARTIDA, SELECAO
+FROM
+(SELECT id_partida AS PARTIDA, nome_selecao_1 AS SELECAO, golos_marcados_selecao1 AS GOLOS
 FROM Partida
-WHERE golos_marcados_selecao1 = golos_marcados_selecao2;
-
----------------------------------------------------------
-CREATE VIEW GolosDefault As
-
-SELECT p.id_partida AS partida, g.nome_selecao AS selecao, COUNT(g.id_evento) AS golos_reais
-FROM (Golo g JOIN Evento e ON (g.id_evento = e.id_evento)) JOIN Partida p ON (e.id_partida = p.id_partida)
-WHERE p.id_partida NOT IN (SELECT * FROM Empates)
-      AND
-      (p.id_partida, g.nome_selecao) NOT IN
-         (SELECT p.id_partida, g.nome_selecao
-         FROM (Golo g JOIN Evento e ON (g.id_evento = e.id_evento)) JOIN Partida p ON (e.id_partida = p.id_partida)
-         WHERE g.tipo = "autogolo")
-         
-GROUP BY p.id_partida, g.nome_selecao;
-
---------------------------------------------------------------------------------------------------------------------
-CREATE VIEW Autogolos As
-
-SELECT B.partida AS partida, B.selecao AS selecao, A.total_golos - B.autogolos AS golos_reais
-FROM
-(SELECT p.id_partida AS partida, g.nome_selecao AS selecao, COUNT(g.id_evento) AS total_golos
-FROM (Golo g JOIN Evento e ON (g.id_evento = e.id_evento)) JOIN Partida p ON (e.id_partida = p.id_partida)
-GROUP BY p.id_partida, g.nome_selecao) AS A,
-
-(SELECT p.id_partida AS partida, g.nome_selecao AS selecao, COUNT(g.id_evento) AS autogolos
-FROM (Golo g JOIN Evento e ON (g.id_evento = e.id_evento)) JOIN Partida p ON (e.id_partida = p.id_partida)
-WHERE g.tipo = "autogolo"
-GROUP BY p.id_partida, g.nome_selecao) AS B
-
-WHERE A.partida = B.partida AND A.selecao = B.selecao AND A.partida NOT IN (SELECT * FROM Empates);
-
----------------------------------------------------------------------------------------------------------------------
-SELECT selecao AS SELECAO, COUNT(*) AS VITÓRIAS
-
-FROM
-(SELECT partida, selecao, MAX(golos_reais)
- FROM
-    (SELECT * FROM GolosDefault
-     UNION
-     SELECT * FROM Autogolos)
- GROUP BY 1)
-
-GROUP BY 1
-ORDER BY 1;
+WHERE golos_marcados_selecao1 <> golos_marcados_selecao2
+UNION
+SELECT id_partida AS PARTIDA, nome_selecao_2 AS SELECAO, golos_marcados_selecao2 AS GOLOS
+FROM Partida
+WHERE golos_marcados_selecao1 <> golos_marcados_selecao2)
+GROUP BY PARTIDA
+HAVING max(GOLOS))
+GROUP BY SELECAO
+ORDER BY SELECAO;
--- int5.sql
+++ int5.sql
@@ -1,16 +1,49 @@
 /*
 Interrogação 5
 --------------
-Liste os jogadores com “r” na terceira ou na antepenúltima posição no nome, que marcaram golos.
-Ordene pelo nome da seleção e nome do jogador.
+Liste o primeiro evento, da segunda parte, da última partida que cada seleção disputou.
+Mostre a data e hora da partida, o nome da seleção, o tipo de evento, e o minuto em que ocorreu.
+Ordene por ordem cronológica.
 */
 
 .mode columns
 .headers on
 .nullvalue NULL
+DROP VIEW IF EXISTS PrimeiroEventoSegundaParte;
+DROP VIEW IF EXISTS UltimaPartida;
 
-Select nome_selecao,numero_jogador,nome_jogador
-from Jogador
-where (nome_jogador like '__r%' or nome_jogador like '%r__')
-and contagem_pessoal <> 0
-order by 1,3
+---------------------------------------------------------
+CREATE VIEW PrimeiroEventoSegundaParte AS
+
+SELECT * 
+FROM
+(SELECT id_partida AS PARTIDA, nome_selecao AS SELECAO, "Cartão" AS TIPO, minuto AS MINUTO
+FROM Evento e JOIN Cartao c ON (e.id_evento = c.id_evento)
+UNION
+SELECT id_partida AS PARTIDA, nome_selecao_entra AS SELECAO, "Substituição" AS TIPO, minuto AS MINUTO
+FROM Evento e JOIN Substituicao s ON (e.id_evento = s.id_evento)
+UNION
+SELECT id_partida as PARTIDA, nome_selecao AS SELECAO, "Golo" AS TIPO, minuto AS MINUTO
+FROM Evento e JOIN Golo g ON (e.id_evento = g.id_evento))
+WHERE MINUTO > 45 AND MINUTO <= 90
+GROUP BY PARTIDA, SELECAO
+HAVING min(MINUTO);
+
+---------------------------------------------------------
+CREATE VIEW UltimaPartida AS
+SELECT *
+FROM
+(SELECT id_partida as PARTIDA, data, hora, nome_selecao_1 AS SELECAO
+FROM Partida
+UNION
+SELECT id_partida as PARTIDA, data, hora, nome_selecao_2 AS SELECAO
+FROM Partida)
+GROUP BY SELECAO
+HAVING max(data)
+ORDER BY PARTIDA;
+
+---------------------------------------------------------
+SELECT B.data AS DATA, B.hora AS HORA, B.SELECAO, A.TIPO, A.MINUTO
+FROM (SELECT * FROM PrimeiroEventoSegundaParte) AS A JOIN (SELECT * FROM UltimaPartida) AS B
+WHERE A.PARTIDA = B.PARTIDA AND A.SELECAO = B.SELECAO
+ORDER BY 1, 2, 5;
